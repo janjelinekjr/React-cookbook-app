f08f3b10f40940a1c6d4a4f64e07c3b99e79fa59
Jan Jelínek <jan.jelinek@unicorn.com>
2022-10-05 12:38:35 +0200

    switch to hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import RecipeList from "./routes/RecipeList";
 import RecipeDetail from "./routes/RecipeDetail";
 import IngredientList from "./routes/IngredientList";
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <FetchDataProvider>
       <UserProvider>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route exact path="/" element={<App />} />
             <Route path="/home" element={<App />} />
@@ -23,7 +23,7 @@ root.render(
             <Route path="/recipeDetail" element={<RecipeDetail />} />
             <Route path="/ingredientList" element={<IngredientList />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </UserProvider>
     </FetchDataProvider>
   </React.StrictMode>
